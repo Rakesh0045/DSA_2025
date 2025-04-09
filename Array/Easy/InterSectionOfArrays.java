5504601a5c66656817695c6347337c6aa9483795
Rakesh Kumar Parida <150229028+Rakesh0045@users.noreply.github.com>
2025-04-09 23:37:23 +0530

    Update InterSectionOfArrays.java

--- Array/Easy/InterSectionOfArrays.java
+++ Array/Easy/InterSectionOfArrays.java
@@ -7,16 +7,45 @@ public class InterSectionOfArrays {
         int i = 0;
         int j = 0;
 
-        while (i < a.length && j < b.length) {
-            if (a[i] == b[j]) {
-                if (Intersection.size() == 0 || Intersection.get(Intersection.size() - 1) != a[i]) {
-                    Intersection.add(a[i]);
-                }
+        // while (i < a.length && j < b.length) {
+        //     if (a[i] == b[j]) {
+        //         if (Intersection.size() == 0 || Intersection.get(Intersection.size() - 1) != a[i]) {
+        //             Intersection.add(a[i]);
+        //         }
+        //         i++;
+        //         j++;
+        //     } else if (a[i] < b[j]) {
+        //         i++;
+        //     } else {
+        //         j++;
+        //     }
+        // }
+
+
+        /*
+         
+            Approach (2 pointer)
+            --------
+
+            * i=0, j=0
+            * Traverse a[], b[] 
+            * If found distinct element a[i]!=b[j] (greater than or less than) then move the corresponding pointer
+            * If a[i] < b[j] --> move i pointer to get bigger element which may be same as b[j]
+            * If b[j] < a[i] --> move j pointer to find bigger element which may be same as a[i]
+            * If found common intersect element i.e a[i]==b[j] then add any of those to intersect<> and move both pointers to find next pair of intersect
+
+
+
+         */
+
+        while(i<a.length && j<b.length){
+            if(a[i]<b[j]){
                 i++;
+            }else if(b[j]<a[i]){
                 j++;
-            } else if (a[i] < b[j]) {
+            }else if(a[i] == b[j]){
+                Intersection.add(a[i]);
                 i++;
-            } else {
                 j++;
             }
         }
